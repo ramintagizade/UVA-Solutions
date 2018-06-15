d5c4973aa2808a3847f8778666aea95fcdab7b38
ramintagizade <ramintagizade@mail.ru>
2018-06-15 21:45:06 +0400

    add solutions

---
+++ src/SuperEasyProblems/Problem12577.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * Problem link :
+ * @see <a href="https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=441&page=show_problem&problem=4022"></a>
+ */
+
+
+public class Problem12577 {
+
+    public static void main (String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int cases = 1;
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            if(line.contains("*")) break;
+
+            if(line.contains("Hajj")) {
+                System.out.println("Case "+ cases + ": Hajj-e-Akbar");
+            }
+            else {
+                System.out.println("Case " +cases + ": Hajj-e-Asghar");
+            }
+            cases++;
+        }
+    }
+}
